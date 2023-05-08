6afbb6d22504c0f3283be1987c343447bcc0a261
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2023-05-08 10:20:13 +0200

    +

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # fmt: off
 import sys, os
 sys.path.append(os.path.abspath('../'))
-from uniform_base import uniform
+from decore_base import decore
 # fmt: on
 
 # TODO - app 'continue' mode - start flask api in thread and trigger main() function. Dokumentation des Verhaltens. 
 if __name__ == '__main__':
-    @uniform.app(p_title='Uniform Sample Application')
+    @decore.app(p_title='Decore Sample Application')
     def main():
         pass
