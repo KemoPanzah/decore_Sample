20d4be2f6c71e683a3be142f8b2f6afa31b2de18
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2023-05-22 13:40:42 +0200

    +

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-# fmt: off
-import sys, os
-sys.path.append(os.path.abspath('../'))
 from decore_base import decore
-# fmt: on
 
-# TODO - app 'continue' mode - start flask api in thread and trigger main() function. Dokumentation des Verhaltens. 
 if __name__ == '__main__':
     @decore.app(p_title='Decore Sample Application')
     def main():
