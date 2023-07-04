b47ab50222642ebfac0ee32ad9af155cd48c9c82
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2023-07-04 13:39:42 +0200

    +

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+# fmt: off
+import sys, os
+sys.path.append(os.path.abspath('../decore_Base'))
+# fmt: on
+
 from decore_base import decore
+from bases import *
+
 
 if __name__ == '__main__':
-    @decore.app(p_title='Decore Sample Application')
+    @decore.app(title='Decore Sample Application')
     def main():
         pass
