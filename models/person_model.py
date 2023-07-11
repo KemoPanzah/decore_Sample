2af08ad77db49b3ca60a35ec7bb4240c8357db8b
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2023-07-11 14:57:58 +0200

    +

--- models/person_model.py
+++ models/person_model.py
@@ -1,6 +1,7 @@
-from decore_base.uniform import *
+from decore_base.uniform.conform_model import *
 from mimesis import Person
 from random import randrange
+from peewee import fn
 
 def get_random_age():
     return Person().age(minimum=16, maximum=64)
@@ -17,4 +18,7 @@ class Person_model(Conform_model):
     age = IntegerField(verbose_name='Age', default=get_random_age)
     capacity = IntegerField(verbose_name='Capacity', default=get_random_capacity)
 
-    
+    @Decore_model.user_query()
+    def count_companies(cls, source_query, operator, value):
+        return_query = source_query.join(cls.companies.through_model.alias()).join(cls.companies.rel_model.alias()).group_by(cls).having(operator(fn.COUNT(cls.companies.rel_model),value))
+        return return_query
