6e35d776153892f3e9a1b55e54d7a9c625f2d365
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2023-08-11 11:21:55 +0200

    +

--- models/contract_model.py
+++ models/contract_model.py
@@ -6,6 +6,6 @@ import datetime
 class Contract_model(Conform_model):
     person = ForeignKeyField(Person_model, backref='contracts', verbose_name='Person')
     position = CharField(verbose_name='Position', default='Developer', choices={'Developer':'Developer','Designer':'Designer','Manager':'Manager','Salesman':'Salesman'})
-    start_date = DateTimeField(verbose_name='Date', default=datetime.date.today)
-    end_date = DateTimeField(null=True, verbose_name='Date')
+    start_date = DateTimeField(verbose_name='Start date', default=datetime.date.today)
+    end_date = DateTimeField(null=True, verbose_name='End date')
     fixed_term = IntegerField(verbose_name='Deadline', default=0, choices={0:'Infinite',1:'1 Year', 2:'2 Years', 3:'3 Years', 4:'4 Years', 5:'5 Years'})
