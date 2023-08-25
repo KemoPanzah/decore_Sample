772dcc40f1dc1b560b71bbb7b84291808b36c1b8
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2023-08-25 15:23:16 +0200

    +

--- models/test_model.py
+++ models/test_model.py
@@ -1,8 +1,12 @@
+import datetime
+
 from decore_base.uniform.conform_model import *
 
 class Test_model(Conform_model):
-    charfield = CharField(verbose_name='CharField', default='Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+    booleanfield = BooleanField(verbose_name='BooleanField', help_text='A field to store boolean values', default=False)
+    charfield = CharField(verbose_name='CharField', help_text="A field to store a char in database", default='Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+    datefield = DateField(verbose_name='DateField', help_text='A field to store a date in database', default=datetime.date.today)
+    datetimefield = DateTimeField(verbose_name='DateTimeField', help_text='A field to store a datetime in database', default=datetime.datetime.now)
     intfield = IntegerField(verbose_name='IntegerField', default=0)
     textfield = TextField(verbose_name='TextField', default='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.')
-    booleanfield = BooleanField(verbose_name='BooleanField', default=False)
     passwordfield = PasswordField(null=True, verbose_name='PasswordField')
