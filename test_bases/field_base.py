78d8b8ecf77d064ecda5ef7f9f3d5c5c512847a8
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2023-09-18 13:39:49 +0200

    +

--- test_bases/field_base.py
+++ test_bases/field_base.py
@@ -18,7 +18,7 @@ class Field_base:
                     else:
                         return False, 'Error'
     
-    @decore.view(title='Field table', icon='mdi-table', type='table', parent_id='Test_base', fields=[Model.boolean, Model.char, Model.date, Model.datetime,Model.float,Model.foreignkey, Model.fk_backrefs, Model.int, Model.manytomany, Model.mm_backrefs ,Model.text, Model.password])
+    @decore.view(title='Field table', icon='mdi-table', type='table', parent_id='Test_base', fields=[Model.boolean, Model.char, Model.date, Model.datetime,Model.float,Model.foreignkey, Model.fk_backrefs, Model.int, Model.manytomany, Model.mm_backrefs ,Model.text, Model.password], filters=[Model.boolean, Model.char, Model.date, Model.datetime,Model.float,Model.foreignkey, Model.fk_backrefs, Model.int, Model.manytomany, Model.mm_backrefs ,Model.text, Model.password])
     def tfb_vi2():
         @decore.dialog(title='Add field data', icon='mdi-table', type='standard', display='drawer', activator='default')
         def tfb_vi2_di1():
@@ -39,4 +39,9 @@ class Field_base:
                     item.title = 'Test_item'
                     item.save()
                     return True, 'Success'
-        
+                
+        @decore.dialog(title='Edit field data', icon='mdi-table', type='standard', display='drawer', activator='click')
+        def tfb_vi2_di3():
+            @decore.widget(title='Field form', icon='mdi-table', type='info', fields=[Model.boolean, Model.char, Model.date, Model.datetime,Model.float,Model.foreignkey, Model.fk_backrefs, Model.int, Model.manytomany, Model.mm_backrefs ,Model.text, Model.password])
+            def tfb_vi2_di3_wi1():
+                pass
