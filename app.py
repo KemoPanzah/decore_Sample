86953b8516d026247caaa27005449b3d8a1f15bb
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2023-11-10 09:01:37 +0100

    +

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ from test_bases import *
 
 
 if __name__ == '__main__':
-    @decore.app(title='Decore Sample Application', allow_guest=False)
+    @decore.app(title='Decore Sample Application', allow_guest=True)
     def main():
         pass
