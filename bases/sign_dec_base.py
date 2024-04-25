a0b85adfaa8e600bbc17257a0733b38514f8f1f0
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2024-04-25 06:32:45 +0200

    +

--- bases/sign_dec_base.py
+++ bases/sign_dec_base.py
@@ -1,5 +1,24 @@
 from decore_base import decore
+from decore_base.library.powershell.powershell import Ps_process, PS_command
 
 @decore.base(title='Sign.dec', icon='mdi-sign-caution', desc='decore Base | Signature Dashboard')
 class Sign_dec_base:
-    pass
+    ps = Ps_process()
+
+    @decore.action(parent_id='sign_dec_user_view', title='Connect EXO', icon='mdi-connection', desc='Connect to Exchange Online', activator='default')
+    def sign_dec_user_view_ac1(self, **kwargs):
+        t_cmd = PS_command('Connect-ExchangeOnline')
+        t_result = self.ps.execute(t_cmd)
+        if t_result.success:
+            return True, 'Connected to Exchange Online'
+        else:
+            return False, t_result.result
+        
+    @decore.action(parent_id='sign_dec_user_view', title='Sync user', icon='mdi-sync', desc='Synchronize online user', activator='default')
+    def sign_dec_user_view_ac2(self, **kwargs):
+        t_cmd = PS_command('Get-User').cmd('Select-Object', Property=['UserPrincipalName','DisplayName', 'RecipientType'])
+        t_result = self.ps.execute(t_cmd)
+        if t_result.success:
+            return True, t_result.result
+        else:
+            return False, t_result.result
--- bases/sign_dec_user_base.py
+++ bases/sign_dec_user_base.py
@@ -1,17 +1,10 @@
 from decore_base import decore
 from models.sign_dec_user_model import Sign_dec_user_model
 
-from decore_base.library.powershell3 import PS_session
 
 @decore.base(title='User', icon='mdi-account', desc='decore Signature Deck - User', model=Sign_dec_user_model)
 class Sign_dec_user_base:
-    ps_session = PS_session()
     
     @decore.view(parent_id='Sign_dec_base', title='User', icon='mdi-account', desc='User', type='table', fields=Sign_dec_user_model.field_s)
     def sign_dec_user_view():
-        
-        @decore.action(title='Get Users', icon='mdi-sync', type='standard', activator='default')
-        def sign_dec_user_view_ac1(self, **kwargs):
-            t_session: PS_session = self.ps_session
-            t_test = t_session.cmd('Connect-ExchangeOnline', ShowBanner=False).exe()
-            return True, 'All Users Loaded'
+        pass
