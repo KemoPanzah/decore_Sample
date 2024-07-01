7ec06b3346b4b24150dfc8cddad22062189c30d3
Kemo <kemo@PROTON-PC-01>
2024-07-01 07:26:05 +0200

    +

--- bases/beershop.py
+++ bases/beershop.py
@@ -2,6 +2,13 @@ from decore_base import decore
 
 @decore.base(title='BeerShop', icon='mdi-beer')
 class Beershop:
+    
+    @decore.dialog(parent_id='app', title='About', icon='mdi-information-outline')
+    def about():
+        @decore.widget(title='About', icon='mdi-information-outline')
+        def about_wi1():
+            pass
+
     @decore.view(title="Dashboard", icon="mdi-view-dashboard", type="default")
     def dashboard():
         pass
